8e642ef529130c6cbab348a23866b2f8853563be
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-04-30 06:13:26 +0000

    added post, comments, likes, shares, and bookmarks + linked them

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -15,5 +15,78 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+    
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    caption: Mapped[str] = mapped_column(String(120), nullable=False)
+    location: Mapped[str] = mapped_column(String(120), nullable=False)
+    tags: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image": self.image,
+            "caption": self.caption,
+            "location": self.location,
+            "tags": self.tags,
+        }
+    
+class Likes(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # user who liked
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    # post liked
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    # comment liked
+    comment_id: Mapped[int] = mapped_column(ForeignKey("comments.id"))
+
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "comment_id": self.comment_id
+        }
+    
+class Comments(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    comment: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "comment": self.comment,
+        }
+    
+class Shares(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
+    
+class bookmarks(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "post_id": self.post_id
         }
